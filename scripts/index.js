edc3384229262697f7a74d000157e9ff6a79051d
Alex Cera <alex@Alexs-MacBook-Pro.local>
2014-07-01 18:16:34 +0800

    Add some styles on the timer. Add the loop and print the remaining time.

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,57 @@
 window.onload = function() {
-    var endDate = new Date(new Date().getTime() + 60 * 60 * 24 * 1000);
-    endDate.setHours(0, 0, 0, 0);
-    startCountDown(el, endDate.getTime());
+
+    //this could be any date in the future. 
+    //just using tomorrow's date as an example;
+    var endDate = new Date(new Date().getTime() + 5000);//(60 * 60 * 24 * 1000));
+   // endDate.setHours(0, 0, 0, 0);
+
+    var timer = new Timer(endDate);
+    
+    var remaining = document.querySelector(".remaining");
+    var label = document.querySelector(".label");
+
+    var intervalId = setInterval(function() {
+        if (!timer.ended()) {
+            remaining["innerHTML" || "textContent"] = timer.remainingTime();
+        } else {
+            remaining["innerHTML" || "textContent"] = "ENDED!";
+            label["innerHTML" || "textContent"] = "";
+            clearInterval(intervalId);
+        }
+    }, 1000);
 };
 
+(function(w) {    
 
-function startCountDown (el, endDate) {
-  
-  var ht = endDate;
+    var Timer = w.Timer = function Timer(endDate) {
+            if (!(this instanceof arguments.callee))
+                return new Timer(endDate);
+            this.endDate = endDate
+            this.end = false;
+        }
 
-  setInterval(function() {
+    Timer.prototype.remainingTime = function remainingTime() { 
 
-  }, 1000);  
-};
+        var secondsInMillis = 1000;
+        var minutesInMillis = secondsInMillis * 60;
+        var hoursInMillis = minutesInMillis * 60;
+        var daysInMillis = hoursInMillis * 24;
+
+        var diff = this.endDate.getTime()
+            - new Date().getTime();
+        
+        var hrs = Math.floor(diff / hoursInMillis);
+        var mins = Math.floor((diff % hoursInMillis) / minutesInMillis);
+        var secs = Math.floor((diff % minutesInMillis) / secondsInMillis);
+
+        if (hrs == 0 && mins == 0 && secs == 0) {
+            this.end = true;
+        }
+
+        return hrs + ":" + mins + ":" + secs;
+    }
+
+    Timer.prototype.ended = function() {
+        return this.end;
+    }
+})(window);
