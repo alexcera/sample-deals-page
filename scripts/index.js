c1a788210becbaa9d1e652ce7d4424b79caf3808
Alex Cera <alex@Alexs-MacBook-Pro.local>
2014-07-02 08:28:58 +0800

    code refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 window.onload = function() {
-
     //this could be any date in the future. 
     //just using tomorrow's date as an example;
     var endDate = new Date(new Date().getTime() + 60 * 60 * 24 * 1000);
-   endDate.setHours(0, 0, 0, 0);
+    endDate.setHours(0, 0, 0, 0);
 
     var timer = new Timer(endDate);
     
@@ -12,19 +11,22 @@ window.onload = function() {
 
     var intervalId = setInterval(function() {
         if (!timer.ended()) {
-            remaining["innerHTML" || "textContent"] = timer.remainingTime();
+            setText(remaining, timer.remainingTime());
             remaining.className = "remaining";
             label.className = "label";
         } else {
-            remaining["innerHTML" || "textContent"] = "ENDED!";
-            label["innerHTML" || "textContent"] = "";
+            setText(remaining, "ENDED!");
+            setText(label, "");
             clearInterval(intervalId);
         }
     }, 1000);
 };
 
-(function(w) {    
+function setText (el, text) {
+    el["innerHTML" || "textContent"] = text;
+}
 
+(function(w) {    
     var Timer = w.Timer = function Timer(endDate) {
             if (!(this instanceof arguments.callee))
                 return new Timer(endDate);
@@ -33,22 +35,20 @@ window.onload = function() {
         }
 
     Timer.prototype.remainingTime = function remainingTime() { 
-
-        var secondsInMillis = 1000;
-        var minutesInMillis = secondsInMillis * 60;
-        var hoursInMillis = minutesInMillis * 60;
-        var daysInMillis = hoursInMillis * 24;
+        var secondsInMillis = 1000,
+            minutesInMillis = secondsInMillis * 60,
+            hoursInMillis = minutesInMillis * 60,
+            daysInMillis = hoursInMillis * 24;
 
         var diff = this.endDate.getTime()
             - new Date().getTime();
         
-        var hrs = Math.floor(diff / hoursInMillis);
+        var hrs = Math.floor(diff / (hoursInMillis));
         var mins = Math.floor((diff % hoursInMillis) / minutesInMillis);
         var secs = Math.floor((diff % minutesInMillis) / secondsInMillis);
 
-        if (hrs == 0 && mins == 0 && secs == 0) {
+        if (hrs == 0 && mins == 0 && secs == 0)
             this.end = true;
-        }
 
         return hrs + ":" + mins + ":" + secs;
     }
