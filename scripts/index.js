47d7a14d579f0f3f989f5c3d06ece94858e8cc4a
Alex Cera <alex@Alexs-MacBook-Pro.local>
2014-07-02 10:37:54 +0800

    delete the assert js and the css. add some code comments.

--- scripts/index.js
+++ scripts/index.js
@@ -20,20 +20,37 @@ window.onload = function() {
             clearInterval(intervalId);
         }
     }, 1000);
+
 };
 
+//browser-related property-detection
 function setText (el, text) {
     el["innerHTML" || "textContent"] = text;
 }
 
+/*
+ * created a Timer object inside a 
+ * closure to protect its properties from outside
+*/
 (function(w) {    
     var Timer = w.Timer = function Timer(endDate) {
+
+            //check if it is a date object, if not, fallback to tomorrow's date.
+            Object.prototype.toString.call(endDate) !== "[object Date]" 
+                ? endDate = new Date(new Date().getTime() + 1000 * 60 * 60 * 24) 
+                : endDate;
+
+            // checks if the Timer is called using its constructor,
+            // if not call explicitly the constructor and return it
             if (!(this instanceof arguments.callee))
                 return new Timer(endDate);
+
             this.endDate = endDate
             this.end = false;
         }
 
+    // returns the remaining time from the endDate
+    // to currentDate in the form of 'hours:minutes:seconds'
     Timer.prototype.remainingTime = function remainingTime() { 
         var secondsInMillis = 1000,
             minutesInMillis = secondsInMillis * 60,
@@ -47,7 +64,7 @@ function setText (el, text) {
         var mins = Math.floor((diff % hoursInMillis) / minutesInMillis);
         var secs = Math.floor((diff % minutesInMillis) / secondsInMillis);
 
-        if (hrs == 0 && mins == 0 && secs == 0)
+        if (hrs <= 0 && mins <= 0 && secs <= 0)
             this.end = true;
 
         return hrs + ":" + mins + ":" + secs;
