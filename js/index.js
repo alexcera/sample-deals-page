9b85676ebd7aaeb56fb985f914265fdc0e1493ab
Alex Cera <alexcera007@gmail.com>
2014-07-04 10:30:29 +0800

    fixed the wrong way of checking for browser-related properties.

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ window.onload = function() {
     endDate.setHours(0, 0, 0, 0);
 
     var timer = new Timer(endDate);
-    
+
     var remaining = document.querySelector(".remaining");
     var label = document.querySelector(".label");
 
@@ -24,42 +24,41 @@ window.onload = function() {
 };
 
 //browser-related property-detection
-function setText (el, text) {
-    el["innerHTML" || "textContent"] = text;
+function setText(el, text) {
+    while (element.firstChild !== null)
+        element.removeChild(element.firstChild);
+    element.appendChild(document.createTextNode(text));
 }
 
 /*
- * created a Timer object inside a 
+ * created a Timer object inside a
  * closure to protect its properties from outside
-*/
-(function(w) {    
+ */
+(function(w) {
     var Timer = w.Timer = function Timer(endDate) {
 
-            //check if it is a date object, if not, fallback to tomorrow's date.
-            Object.prototype.toString.call(endDate) !== "[object Date]" 
-                ? endDate = new Date(new Date().getTime() + 1000 * 60 * 60 * 24) 
-                : endDate;
+        //check if it is a date object, if not, fallback to tomorrow's date.
+        Object.prototype.toString.call(endDate) !== "[object Date]" ? endDate = new Date(new Date().getTime() + 1000 * 60 * 60 * 24) : endDate;
 
-            // checks if the Timer is called using its constructor,
-            // if not call explicitly the constructor and return it
-            if (!(this instanceof arguments.callee))
-                return new Timer(endDate);
+        // checks if the Timer is called using its constructor,
+        // if not call explicitly the constructor and return it
+        if (!(this instanceof arguments.callee))
+            return new Timer(endDate);
 
-            this.endDate = endDate
-            this.end = false;
-        }
+        this.endDate = endDate
+        this.end = false;
+    }
 
     // returns the remaining time from the endDate
     // to currentDate in the form of 'hours:minutes:seconds'
-    Timer.prototype.remainingTime = function remainingTime() { 
+    Timer.prototype.remainingTime = function remainingTime() {
         var secondsInMillis = 1000,
             minutesInMillis = secondsInMillis * 60,
             hoursInMillis = minutesInMillis * 60,
             daysInMillis = hoursInMillis * 24;
 
-        var diff = this.endDate.getTime()
-            - new Date().getTime();
-        
+        var diff = this.endDate.getTime() - new Date().getTime();
+
         var hrs = Math.floor(diff / (hoursInMillis));
         var mins = Math.floor((diff % hoursInMillis) / minutesInMillis);
         var secs = Math.floor((diff % minutesInMillis) / secondsInMillis);
